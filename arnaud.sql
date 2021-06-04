145452df5a747c872f6590c1b0e554d3c349572d
ArnoCaillol <arnaud.caillol@tuta.io>
2021-06-04 16:39:00 +0200

    Ajout de livres multimedia

--- arnaud.sql
+++ arnaud.sql
@@ -1,13 +1,17 @@
+USE fil_rouge;
+
 insert into auteur (nom, prenom) 
 values (
     'Moreau', 
     'Philippe'
 );
 
-insert into genre values ('bureautique');
+insert into genre values ('bureautique'), ('multimedia');
 
 insert into editeur values ('Eyrolles');
 
+
+-- articles bureautique
 insert into article (titre, resume, prix, stock) values ('Excel 2013 Initiation', 
 "Maîtrisez rapidement les fonctions essentielles d'Excel 2013 pour créer vos propres feuilles de calcul", 
 10,
@@ -28,12 +32,37 @@ insert into article (titre, resume, prix, stock) values ('Excel 2013 Initiation'
 "Maîtrisez rapidement les fonctions essentielles de Paint pour faire vos cours d'Angular en distanciel",
 10,
 0);
+
+-- articles multimedia
+insert into article (titre, resume, prix, stock) values ('Photoshop Initiation', 
+"Maîtrisez rapidement ce logiciel propriétaire très cher pour vous faire du lifting sur vos photos", 
+10,
+3),
+('Windows Movie Maker Initiation',
+"Maîtrisez rapidement Windows Movie Maker pour faire vos montages Naruto sur du Linkin Park en 144p",
+10,
+5),
+('Audacity Initiation',
+"Maîtrisez rapidement Audacity afin d'enregistrer des reprises de vos chansons préférées",
+10,
+1),
+('FL Studio Initiation',
+"Prenez-vous pour un grand producteur de trap en créant des beats en carton avec FL Studio",
+10,
+7),
+('MPV Initiation',
+"Découvrez ce lecteur multimédia extrêmement léger et extensible",
+10,
+0);
+
 -- ref INT PRIMARY KEY,
 --     titre VARCHAR(100),
 --     resume TEXT,
 --     prix INT(10),
 --     stock INT(150)
 
+
+-- livres bureautique
 insert into livre values('9782212138115', 'excel.png', 'poche', 'Eyrolles', 
 (select ref from article where titre = 'Excel 2013 Initiation'), 
 'bureautique'),
@@ -50,6 +79,23 @@ insert into livre values('9782212138115', 'excel.png', 'poche', 'Eyrolles',
 (select ref from article where titre = 'Paint Initiation'), 
 'bureautique');
 
+-- livres multimedia
+insert into livre values('9154357854552', 'ps.png', 'poche', 'Eyrolles', 
+(select ref from article where titre = 'Photoshop Initiation'), 
+'multimedia'),
+('9736912194826', 'wmm.png', 'poche', 'Eyrolles', 
+(select ref from article where titre = 'Windows Movie Maker Initiation'), 
+'multimedia'),
+('9776481594824', 'audacity.png', 'poche', 'Eyrolles', 
+(select ref from article where titre = 'Audacity Initiation'), 
+'multimedia'),
+('9782216485529', 'fl.png', 'poche', 'Eyrolles', 
+(select ref from article where titre = 'FL Studio Initiation'), 
+'multimedia'),
+('9758155469976', 'mpv.png', 'poche', 'Eyrolles', 
+(select ref from article where titre = 'MPV Initiation'), 
+'multimedia');
+
 -- ISBN VARCHAR(50) PRIMARY KEY,
 --     imageLivre VARCHAR(50),
 --     formatLivre VARCHAR(40),
@@ -60,6 +106,8 @@ insert into livre values('9782212138115', 'excel.png', 'poche', 'Eyrolles',
 --     CONSTRAINT fk_genre FOREIGN KEY (nomGenre) REFERENCES genre(nom),
 --     CONSTRAINT fk_editeur FOREIGN KEY (nomEditeur) REFERENCES editeur(nom)
 
+
+-- auteurLivre bureautique
 insert into auteurLivre (idAuteur, ISBN) values (
     (select id from auteur where nom = 'Moreau' and prenom = 'Philippe'),
     '9782212138115'
@@ -80,6 +128,29 @@ insert into auteurLivre (idAuteur, ISBN) values (
     (select id from auteur where nom = 'Moreau' and prenom = 'Philippe'),
     '9782212193576'
 );
+
+-- auteurLivre multimedia
+insert into auteurLivre (idAuteur, ISBN) values (
+    (select id from auteur where nom = 'Moreau' and prenom = 'Philippe'),
+    '9154357854552'
+),
+(
+    (select id from auteur where nom = 'Moreau' and prenom = 'Philippe'),
+    '9736912194826'
+),
+(
+    (select id from auteur where nom = 'Moreau' and prenom = 'Philippe'),
+    '9776481594824'
+),
+(
+    (select id from auteur where nom = 'Moreau' and prenom = 'Philippe'),
+    '9782216485529'
+),
+(
+    (select id from auteur where nom = 'Moreau' and prenom = 'Philippe'),
+    '9758155469976'
+);
+
 -- id INT PRIMARY KEY AUTO_INCREMENT,
 -- idAuteur INT,
 -- ISBN VARCHAR(50),
